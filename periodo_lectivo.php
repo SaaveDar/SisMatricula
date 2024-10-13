eb2dc216f40dc0a182f06eb5904766a5593d16f8
DarleyEvans <darleysaavedra@gmail.com>
2024-10-12 20:51:49 -0500

    programa de estudios y periodo lectivo

---
+++ periodo_lectivo.php
@@ -0,0 +1,173 @@
+<?php
+session_start();
+if (empty($_SESSION["nombre"]) and empty($_SESSION["apellido"])) {
+	header("location:login.php");
+} 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/estilo.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo $URL;?>/public/css/bootstrap.min.css">
+</head>
+<body>
+<!--	<div class="bd-example mb-0" style="height: 80vh">
+		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active" style="height: 80vh">
+					<img src="img/1.jpg" class="d-block w-100" alt="...">
+					<div class="carousel-caption d-none d-md-block">
+						
+					</div>
+				</div>
+				<div class="carousel-item" style="height: 80vh">
+					<img src="img/1.jpg" class="d-block w-100" alt="...">
+					<div class="carousel-caption d-none d-md-block">
+						
+					</div>
+				</div>
+				<div class="carousel-item" style="height: 80vh">
+					<img src="img/1.jpg" class="d-block w-100" alt="...">
+					<div class="carousel-caption d-none d-md-block">
+					
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>-->
+
+	<nav class="navbar navbar-dark bg-dark  navbar-expand-md navbar-light bg-light fixed-top">
+		<div class="text-white bg-success p-2">
+			<?php
+			echo "Bienvenido ".$_SESSION["nombre"]." ".$_SESSION["apellido"];
+			?>
+		</div>
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+			<div class="navbar-nav mr-auto">
+				<div class="offset-md-1 mr-auto text-center"></div>
+						
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle text-justify active ml-3 hover-primary" href="" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Registrar
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#">Postulante</a>
+						<a class="dropdown-item" href="#">Estudiante</a>
+						<a class="dropdown-item" href="#">Periodo Lectivo</a>
+						<a class="dropdown-item" href="#">Programa de Estudios</a>
+						<a class="dropdown-item" href="#">Plan de Estudios</a>
+						<a class="dropdown-item" href="#">Unidades Didacticas</a>
+						<a class="dropdown-item" href="#">Periodo Academico</a>
+						<a class="dropdown-item" href="#">Tupa</a>
+						<a class="dropdown-item" href="#">Usuario</a>
+					</div>
+				</li>
+				<a class="nav-item nav-link text-justify ml-3 hover-primary" href="#">Matricular</a>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle text-justify ml-3" href="" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Reporte
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#">Postulante</a>
+						<a class="dropdown-item" href="#">Estudiante</a>
+						<a class="dropdown-item" href="#">Matriculas</a>
+						<a class="dropdown-item" href="#">Estado de cuenta</a>
+					</div>
+				</li>
+				<a class="nav-item nav-link text-justify ml-3 hover-primary" href="controladorCerrar.php">Salir</a>
+			</div>
+			<div class="text-center justify-content-center">
+				<a class="btn btn-outline-primary" target="_blank" href="https://www.facebook.com/istelaredo.trujillo.7">Facebook</a>
+				<a class="btn btn-outline-danger" target="_blank" href="https://istelaredo.edu.pe">www.istelaredo.edu.pe</a>
+			</div>
+		</div>
+
+	</nav>
+	<main>
+	<div class="row">
+        <div class="container" style="width: 95%">
+            <br>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary" style="box-shadow: 0px 5px 5px 5px #c0c0c0">
+                        <div class="card-header">
+                            Registrar datos del periodo académico
+                        </div>
+                        <div class="card-body">
+                            <form action="controller_create.php" method="post" enctype="multipart/form-data">
+                                <div class="row">
+									<div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="">NOMBRE DEL PERIODO ACADÉMICO</label>
+                                            <input type="text" name="nombre_periodo_academico" class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <hr>
+                                <center>
+                                    <button type="submit" class="btn btn-primary btn-lg" onclick="return confirm('Por favor revisa bien los datos antes de enviar');">
+                                        <i class="fa fa-save"></i> Registrar periodo académico
+                                </center>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <br>
+        </div>
+    </div>
+	</main>
+	<div class="">
+		<div class="jumbotron bg-dark text-light rounded-0">
+        <!--Pie de pagina-->
+		<p> © 2024 IESTP LAREDO | Reservados todos los derechos</p>
+		</div>
+	</div>
+	</div>
+
+<!--
+	<form action="" class="form-inline d-flex justify-content-center flex-column flex-md-row">
+		<div class="form-group mx-2 my-2">
+			<label class="d-none d-md-block" for="">Nombre</label>
+			<input type="text" class="form-control" placeholder="Nombre">
+		</div>
+		<div class="form-group mx-2 my-2">
+			<label class="d-none d-md-block" for="">Apellido</label>
+			<input type="text" class="form-control" placeholder="Apellido">
+		</div>
+		<div class="form-group mx-2 my-2">
+			<button class="btn btn-outline-primary">enviar</button>
+		</div>
+	</form>
+
+-->
+
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+</body>
+
+</html>
